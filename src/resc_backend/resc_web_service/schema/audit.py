05e99398d8b4df2ee9721decd34520e1b546f8c1
Benoît Viguier <ildyria@users.noreply.github.com>
2024-06-21 09:12:17 +0200

    Missing 1000 (#66)

--- src/resc_backend/resc_web_service/schema/audit.py
+++ src/resc_backend/resc_web_service/schema/audit.py
@@ -5,11 +5,12 @@ import datetime
 from pydantic import BaseModel, conint, conlist, constr
 
 # First Party
+from resc_backend.constants import MAX_RECORDS_PER_PAGE_LIMIT
 from resc_backend.resc_web_service.schema.finding_status import FindingStatus
 
 
 class AuditMultiple(BaseModel):
-    finding_ids: conlist(conint(gt=0), min_items=1, max_items=500)
+    finding_ids: conlist(conint(gt=0), min_items=1, max_items=MAX_RECORDS_PER_PAGE_LIMIT)
     status: FindingStatus
     comment: constr(max_length=255)
 
